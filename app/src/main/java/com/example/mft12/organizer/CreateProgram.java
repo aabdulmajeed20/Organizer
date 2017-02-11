e495dd3f47128d0fde1048330e4b5e5f41464d28
Abdulmajeed Altaweel <Abdulmajeed Altaweel>
2017-02-11 08:19:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mft12/organizer/CreateProgram.java
@@ -0,0 +1,20 @@
+package com.example.mft12.organizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class CreateProgram extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_program);
+
+
+
+
+    }
+}
